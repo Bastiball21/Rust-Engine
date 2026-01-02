d0b5fb4b8c368bb96bb58085f0d031fc1805f806
Basti Dangca <135342382+Bastiball21@users.noreply.github.com>
2026-01-02 17:12:13 +0800

    Fix NNUEScratch struct sizes for hidden_l1 and final_out
    Reduced hidden_l1 size from L1_SIZE * 2 to L1_SIZE and updated final_out to use OUTPUT_SIZE constant.

--- src/nnue_scratch.rs
+++ src/nnue_scratch.rs
@@ -1,23 +1,22 @@
-
-use crate::nnue::{L1_SIZE, L2_SIZE, L3_SIZE, L4_SIZE};
+use crate::nnue::{L1_SIZE, L2_SIZE, L3_SIZE, L4_SIZE, OUTPUT_SIZE};
 
 #[repr(align(64))]
 pub struct NNUEScratch {
-    pub hidden_l1: [i16; L1_SIZE * 2],
+    pub hidden_l1: [i16; L1_SIZE], // Fixed: Reduced from L1_SIZE * 2 to L1_SIZE
     pub l2_out: [i16; L2_SIZE],
     pub l3_out: [i16; L3_SIZE],
     pub l4_out: [i16; L4_SIZE],
-    pub final_out: [i16; 1],
+    pub final_out: [i16; OUTPUT_SIZE], // Fixed: Used constant instead of literal 1
 }
 
 impl Default for NNUEScratch {
     fn default() -> Self {
         Self {
-            hidden_l1: [0; L1_SIZE * 2],
+            hidden_l1: [0; L1_SIZE],
             l2_out: [0; L2_SIZE],
             l3_out: [0; L3_SIZE],
             l4_out: [0; L4_SIZE],
-            final_out: [0; 1],
+            final_out: [0; OUTPUT_SIZE],
         }
     }
 }
